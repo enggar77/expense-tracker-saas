7f044cb3e85fb65f6adf3b9d56a8eb7cdf219908
enggar <ejihandoko@gmail.com>
2025-01-31 16:54:13 +0700

    set up project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Expenses Tracker",
+	description: "Track your expenses with ease!",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-200`}>{children}</body>
+		</html>
+	);
+}
